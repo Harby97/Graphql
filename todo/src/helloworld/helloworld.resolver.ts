eb87c3bb3ac13e154f789316a4999d7ddc231d04
Harby <ingharby@gmail.com>
2023-11-16 16:42:09 -0500

    endpoint con argumentos de entrada

--- todo/src/helloworld/helloworld.resolver.ts
+++ todo/src/helloworld/helloworld.resolver.ts
@@ -1,9 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloworldResolver {
-    @Query(() => String)
+    @Query(() => String, { description: 'Is the first resolver in Graphql', name: 'First_program' })
     helloworld(): string {
         return 'Hola Mundo'
+    }
+
+    @Query(() => Float, { description: 'This resolver return a random number', name: 'Random_Number' })
+    getRandomNumber(): number {
+        return Math.random()
+    }
+
+    @Query(() => Int, { name: 'Random_From_Zero_To' })
+    getRandomFromZeroTo(@Args('to', { nullable: true, type: () => Int }) to: number = 10): number {
+        return Math.floor(Math.random() * to)
     }
 }
