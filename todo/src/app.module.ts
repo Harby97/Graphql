ebab9dade0e0e4201639ec216b2bd3a594cad1c8
Harby <ingharby@gmail.com>
2023-11-16 14:55:14 -0500

    instalacion apollo y configuracion

--- todo/src/app.module.ts
+++ todo/src/app.module.ts
@@ -3,12 +3,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { HelloworldModule } from './helloworld/helloworld.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloworldModule,
   ],
